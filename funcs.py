523bc88a819d8caef94928c1396f15d81ef4aebd
Razvan Anghel <csneed@Razvans-MacBook-Air.local>
2024-06-04 01:34:12 -0700

    working on MSE cost function implementation

--- funcs.py
+++ funcs.py
@@ -6,5 +6,23 @@ class ActivationFunctions:
         return 1 / (1 + math.exp(-x))
     
     @staticmethod
+    def sigmoid_derivative(x) -> float:
+        ActivationFunctions.sigmoid(x) * (1 - ActivationFunctions.sigmoid(x))
+    
+    @staticmethod
     def reLu(x) -> float:
         return max(0, x)
+
+class CostFunctions:
+    # utility function
+    @staticmethod
+    def mse_node_cost(outputActivation: float, expectedOutput) -> float:
+        error = outputActivation - expectedOutput
+        return error ** 2
+    
+    @staticmethod
+    def mse_cost(nn, datapoint) -> float:
+        output = nn.forward(datapoint)
+        cost = 0
+
+        # iterate over outputs to get MSE
